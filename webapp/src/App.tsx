481dffc23f26b9cc5097d8f86b5564a716f5fff8
Kristina <kamagorovakristina@gmail.com>
2025-01-24 00:14:17 +0700

    Start backend, tsc init

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,11 +1,11 @@
 export const App = () => {
   const news = [
-    { nick: "build news 1", name: "Новость 1", description: "Описание 1" },
-    { nick: "build news 2", name: "Новость 2", description: "Описание 2" },
-    { nick: "build news 3", name: "Новость 3", description: "Описание 3" },
-    { nick: "build news 4", name: "Новость 4", description: "Описание 4" },
-    { nick: "build news 5", name: "Новость 5", description: "Описание 5" },
-  ];
+    { nick: 'build news 1', name: 'Новость 1', description: 'Описание 1' },
+    { nick: 'build news 2', name: 'Новость 2', description: 'Описание 2' },
+    { nick: 'build news 3', name: 'Новость 3', description: 'Описание 3' },
+    { nick: 'build news 4', name: 'Новость 4', description: 'Описание 4' },
+    { nick: 'build news 5', name: 'Новость 5', description: 'Описание 5' },
+  ]
   return (
     <div>
       <h1>Home, sweet home!</h1>
@@ -15,8 +15,8 @@ export const App = () => {
             <h2>{text.name}</h2>
             <p>{text.description}</p>
           </div>
-        );
+        )
       })}
     </div>
-  );
-};
+  )
+}
