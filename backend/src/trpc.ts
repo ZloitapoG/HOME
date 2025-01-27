b13fc9a4eede8974ead0e1d3f514b404279cb777
Kristina <kamagorovakristina@gmail.com>
2025-01-27 23:31:04 +0700

    Add tRPC to backend

---
+++ backend/src/trpc.ts
@@ -0,0 +1,16 @@
+import { initTRPC } from '@trpc/server'
+const news = [
+  { nick: 'build news 1', name: 'Новость 1', description: 'Описание 1' },
+  { nick: 'build news 2', name: 'Новость 2', description: 'Описание 2' },
+  { nick: 'build news 3', name: 'Новость 3', description: 'Описание 3' },
+  { nick: 'build news 4', name: 'Новость 4', description: 'Описание 4' },
+  { nick: 'build news 5', name: 'Новость 5', description: 'Описание 5' },
+]
+const trpc = initTRPC.create()
+
+export const trpcRouter = trpc.router({
+  getNews: trpc.procedure.query(() => {
+    return { news }
+  }),
+})
+export type trpcRouter = typeof trpcRouter
