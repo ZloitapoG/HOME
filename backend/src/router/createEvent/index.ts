1278b4f2e1dd74524ab483addddcfce610a3a6b2
Kristina <kamagorovakristina@gmail.com>
2025-03-28 23:59:07 +0700

    Forms: download, check, error, input,textarea, alert

--- backend/src/router/createEvent/index.ts
+++ backend/src/router/createEvent/index.ts
@@ -3,6 +3,9 @@ import { trpc } from '../../lib/trpc.js'
 import { zCreateEventTrpcInput } from './input.js'
 
 export const createEventTrpcRoute = trpc.procedure.input(zCreateEventTrpcInput).mutation(({ input }) => {
+  if (news.find((text) => text.nick === input.nick)) {
+    throw Error('Баян!')
+  }
   news.unshift(input)
   return true
 })
--- webapp/src/pages/NewTextPage/index.tsx
+++ webapp/src/pages/NewTextPage/index.tsx
@@ -1,12 +1,16 @@
 import { zCreateEventTrpcInput } from '@home/backend/src/router/createEvent/input'
 import { useFormik } from 'formik'
 import { withZodSchema } from 'formik-validator-zod'
+import { useState } from 'react'
+import { Alert } from '../../components/Alert'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
 import { Textarea } from '../../components/Textarea'
 import { trpc } from '../../lib/trpc'
 
 export const NewTextPage = () => {
+  const [successMassageVisible, setSuccessMassageVisible] = useState(false)
+  const [submittingError, setsubmittingError] = useState<string | null>(null)
   const createText = trpc.createEvent.useMutation()
   const formik = useFormik({
     initialValues: {
@@ -17,7 +21,20 @@ export const NewTextPage = () => {
     },
     validate: withZodSchema(zCreateEventTrpcInput),
     onSubmit: async (values) => {
-      await createText.mutateAsync(values)
+      try {
+        await createText.mutateAsync(values)
+        formik.resetForm()
+        setSuccessMassageVisible(true)
+        setTimeout(() => {
+          setSuccessMassageVisible(false)
+        }, 3000)
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error: any) {
+        setsubmittingError(error.message)
+        setTimeout(() => {
+          setsubmittingError(null)
+        }, 3000)
+      }
     },
   })
   return (
@@ -30,10 +47,14 @@ export const NewTextPage = () => {
       >
         <Input name="name" label="Заголовок" formik={formik} />
         <Input name="nick" label="В области" formik={formik} />
-        <Input name="description" label="Кратко о событии" formik={formik} />
+        <Input name="description" label="Кратко о событии" formik={formik} maxWidth={500} />
         <Textarea name="text" label="Ну, рассказывай:" formik={formik} />
         {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>Где то скрывается косяк о_О</div>}
-        <button type="submit">Субмитить</button>
+        {!!submittingError && <Alert color="red">{submittingError}</Alert>}
+        {successMassageVisible && <Alert color="green">Событие опубликовано!</Alert>}
+        <button type="submit" disabled={formik.isSubmitting}>
+          {formik.isSubmitting ? 'Субмититься...' : 'Засубмитить'}
+        </button>
       </form>
     </Segment>
   )
