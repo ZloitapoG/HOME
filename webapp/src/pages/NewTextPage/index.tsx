05281ce8c20f03dd8a53d6893b12e8bc239f5737
Kristina <kamagorovakristina@gmail.com>
2025-03-29 11:25:20 +0700

    Button and margin-top

--- webapp/src/pages/NewTextPage/index.tsx
+++ webapp/src/pages/NewTextPage/index.tsx
@@ -3,6 +3,8 @@ import { useFormik } from 'formik'
 import { withZodSchema } from 'formik-validator-zod'
 import { useState } from 'react'
 import { Alert } from '../../components/Alert'
+import { Button } from '../../components/Button'
+import { FormItems } from '../../components/FormItem'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
 import { Textarea } from '../../components/Textarea'
@@ -45,16 +47,16 @@ export const NewTextPage = () => {
           formik.handleSubmit()
         }}
       >
-        <Input name="name" label="Заголовок" formik={formik} />
-        <Input name="nick" label="В области" formik={formik} />
-        <Input name="description" label="Кратко о событии" formik={formik} maxWidth={500} />
-        <Textarea name="text" label="Ну, рассказывай:" formik={formik} />
-        {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>Где то скрывается косяк о_О</div>}
-        {!!submittingError && <Alert color="red">{submittingError}</Alert>}
-        {successMassageVisible && <Alert color="green">Событие опубликовано!</Alert>}
-        <button type="submit" disabled={formik.isSubmitting}>
-          {formik.isSubmitting ? 'Субмититься...' : 'Засубмитить'}
-        </button>
+        <FormItems>
+          <Input name="name" label="Заголовок" formik={formik} />
+          <Input name="nick" label="В области" formik={formik} />
+          <Input name="description" label="Кратко о событии" formik={formik} maxWidth={500} />
+          <Textarea name="text" label="Ну, рассказывай:" formik={formik} />
+          {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>Где то скрывается косяк о_О</div>}
+          {!!submittingError && <Alert color="red">{submittingError}</Alert>}
+          {successMassageVisible && <Alert color="green">Событие опубликовано!</Alert>}
+          <Button loading={formik.isSubmitting}>Засубмитить</Button>
+        </FormItems>
       </form>
     </Segment>
   )
